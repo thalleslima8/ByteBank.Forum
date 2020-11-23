362e4a085e74e21a43a72e438654972e5682144d
Thalles Veloso <thalleslima8@gmail.com>
2020-11-23 10:39:42 -0300

    Iniciando projeto ASP.NET Framework Identity; Simulação de contas e usuários em um banco ByteBank

---
+++ Controllers/ContaController.cs
@@ -0,0 +1,24 @@
+﻿using ByteBank.Forum.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ByteBank.Forum.Controllers
+{
+    public class ContaController : Controller
+    {
+        // GET: Conta
+        public ActionResult Registrar()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Registrar(ContaRegistrarViewModel modelo)
+        {
+            return View();
+        }
+    }
+}
